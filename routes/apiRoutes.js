0e8c5cee8fb91ffdc423652361bd9e361659a256
cfcrotty <carafeliselana@gmail.com>
2019-07-10 14:33:32 -0700

    update from original textreviews

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -86,7 +86,7 @@ module.exports = app => {
           city: 'La Jolla',
           state: 'CA',
           zipCode: '92037',
-          phonenumber: '+12405466255'
+          phonenumber: '+16193044042'
         },
         {
           locationName: 'Atlantic',
