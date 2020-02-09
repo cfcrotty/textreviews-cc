26cf711693f0c8eb38a35976eda39c99ab22dde3
cfcrotty <carafeliselana@gmail.com>
2020-02-08 22:58:20 -0800

    fix for heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/appDB', {useNewUrlParser: true, useCreateIndex: true})
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/appDB', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
   .then(() => console.log("MongoDB Connected!"))
   .catch(err => console.error(err));
 
